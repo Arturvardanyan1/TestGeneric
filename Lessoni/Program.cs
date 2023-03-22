ead7ef65fec5b29f6e7bfdf9146f3160c05df2be
arturvardanyan442@gmail.com <arturvardanyan442@gmail.com>
2023-03-22 23:33:49 +0400

    Add project files.

---
+++ Lessoni/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Lessoni
+{
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            bool mybool=false;
+            TestClass<string> testClass = new TestClass<string>();  
+            testClass.Meth("fhf",ref mybool);
+            testClass.Method(ref mybool);
+           
+                            
+        }
+        
+        
+    }
+}
---
+++ Lessoni/TestClass.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lessoni
+{
+    internal class TestClass<T>
+    {
+        
+        public T Meth<T>(T value,ref bool mybool)
+        {
+            if(value is string){
+                mybool = true;
+            }
+            return value;
+
+        }
+
+        public void Method(ref bool mybool)
+        {
+            if (mybool)
+            {
+                Console.WriteLine(mybool);
+            }
+            else
+            {
+                Console.WriteLine(mybool);
+            }
+        }
+    }
+}
